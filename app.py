0d8e3e9103d06315446d61638618ce785c2c03f4
nina <ninacussac06@gmail.com>
2024-12-17 23:13:35 -0500

    test

--- app.py
+++ app.py
@@ -54,11 +54,6 @@ def long_running():
     time.sleep(5)  # simulate long processing
     return 'This request took a long time!'
 
-@app.route('/metrics')
-def metrics_endpoint():
-    from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
-    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
-
 @app.route('/status/<int:status>')
 @metrics.do_not_track()
 @metrics.summary('requests_by_status', 'Request latencies by status',
